472903fd5c57390d520bbf3201b9a1f56c7bcf95
razik anass <razik.anas@gmail.com>
2025-04-13 19:18:43 +0700

    initial clean maven project setup

---
+++ src/main/java/com/anymind/App.java
@@ -0,0 +1,13 @@
+package com.anymind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
